6fb2d451efe5a807839a03b641dcd23a91b04a46
elatfar <atmam58@gmail.com>
2021-01-07 23:55:26 +0700

    perubahan realworldio

--- cypress/integration/examples/test_realworldio.spec.js
+++ cypress/integration/examples/test_realworldio.spec.js
@@ -1,28 +1,37 @@
 describe('Test on demo.realworld.io', ()=>{
+
+    const randomWaitTime = Math.floor((Math.random() * 10) + 1500);
     
     it('Visiting website and login',()=>{
         cy.viewport('macbook-11')
+        cy.wait(randomWaitTime);
+        cy.clearCookies();
         cy.visit('demo.realworld.io')
+        cy.wait(randomWaitTime);
         cy.contains('Sign in').click()
         cy.get('input[placeholder*="Email"]').type('mawt@gmail.com')
+        cy.wait(3020)
         cy.get('input[placeholder*="Password"]').type('mawt12345')
         cy.get('button[class*="btn btn-lg btn-primary pull-xs-right ng-binding"]').click()
-        cy.wait(3000)
+        cy.wait(2000)
     })
 
     it('Exploring global feed ..',()=>{
         cy.contains('Global Feed').click()
+        cy.wait(randomWaitTime);
         cy.get('article-list button').then(listOfbuttons => {
             var i;
             for (i = 0;i<5;i++){
                 listOfbuttons[i].click()
             }
         })
+        cy.wait(randomWaitTime);
     })
 
     it('Create an article .. ',()=>{
         cy.wait(3000)
-        cy.get('a[ui-sref*="app.editor"]').click()
+        //cy.get('a[ui-sref*="app.editor"]').click()
+        cy.visit('http://demo.realworld.io/#/editor/')
         cy.wait(3000)
         cy.get('input[placeholder*="Article Title"]').type('Tentang Judul')
         cy.get('input[placeholder*="What\'s this article about?"]').type('Membahas sebuah judul')
