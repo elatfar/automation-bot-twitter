bdf77bd5d65fe9e6e41464f6f5c59e3441fb2c4e
elatfar <atmam58@gmail.com>
2021-01-08 16:39:09 +0700

    Perubahan menjadi lokal

--- cypress/integration/examples/test_realworldio.spec.js
+++ cypress/integration/examples/test_realworldio.spec.js
@@ -6,20 +6,20 @@ describe('Test on demo.realworld.io', ()=>{
         cy.viewport('macbook-11')
         cy.wait(randomWaitTime);
         cy.clearCookies();
-        cy.visit('demo.realworld.io')
+        cy.visit('http://localhost:4100/')
         cy.wait(randomWaitTime);
         cy.contains('Sign in').click()
         cy.get('input[placeholder*="Email"]').type('mawt@gmail.com')
         cy.wait(3020)
         cy.get('input[placeholder*="Password"]').type('mawt12345')
-        cy.get('button[class*="btn btn-lg btn-primary pull-xs-right ng-binding"]').click()
+        cy.get('button[class*="btn btn-lg btn-primary pull-xs-right"]').click()
         cy.wait(2000)
     })
 
     it('Exploring global feed ..',()=>{
         cy.contains('Global Feed').click()
         cy.wait(randomWaitTime);
-        cy.get('article-list button').then(listOfbuttons => {
+        cy.get('div button').then(listOfbuttons => {
             var i;
             for (i = 0;i<5;i++){
                 listOfbuttons[i].click()
@@ -30,19 +30,19 @@ describe('Test on demo.realworld.io', ()=>{
 
     it('Create an article .. ',()=>{
         cy.wait(3000)
-        //cy.get('a[ui-sref*="app.editor"]').click()
-        cy.visit('http://demo.realworld.io/#/editor/')
+        cy.get('a[href*="/editor"]').click()
+        //cy.visit('http://demo.realworld.io/#/editor/')
         cy.wait(3000)
         cy.get('input[placeholder*="Article Title"]').type('Tentang Judul')
         cy.get('input[placeholder*="What\'s this article about?"]').type('Membahas sebuah judul')
-        cy.get('input[placeholder*="Write your article (in markdown)"]').type('Sebenarnya apasih judul, apa guna judul?')
+        cy.get('textarea[placeholder*="Write your article (in markdown)"]').type('Sebenarnya apasih judul, apa guna judul?')
         cy.get('input[placeholder*="Enter tags"]').type('title{enter}judul{enter}philosophy{enter}')
         cy.contains('Publish Article').click()
 
     })
 
     it('Find setting and then log out ..', ()=>{
-        cy.get('a[href*="#/settings"]').click()
-        cy.contains('Or click here to logout').click()
+        cy.get('a[href*="/settings"]').click()
+        cy.get('button[class*="btn btn-outline-danger"]').click()
     })
 })
